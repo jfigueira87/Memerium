2713c91209c8dc711eebd31964d5981d34e8aff2
jfigueira87 <jfigueira@gmail.com>
2024-09-03 12:47:09 +0200

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+//Agregue las rutas a todos los archivos de plantilla en su tailwind.config.js archivo.
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
