b1449a8bc975a417fc0ebac194699b72fc1d33ce
jfigueira87 <jfigueira@gmail.com>
2024-09-05 12:44:24 +0200

    Add:Create function getAllMemes()

--- src/services/services.js
+++ src/services/services.js
@@ -1,1 +1,24 @@
-//SERVICES
+import axios from "axios";
+const URL_API = "http://localhost:3000/";
+
+//Get all memes -- GET
+
+async function getAllMemes() {
+  try {
+    const response = await axios.get(URL_API);
+    console.log(response.data); 
+  } catch (error) {
+    console.error('Error en la solicitud:', error);
+  }
+}
+
+getAllMemes();
+
+
+//Get one meme by ID -- GET
+
+//Create meme -- POST
+
+//Delete meme -- DELETE
+
+//Update meme -- PUT
