34d90d15bb81a15115ed871b23c78deb7e47b303
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-09-06 16:19:38 +0200

    feat: formulario para nuevo meme

--- src/pages/CreateMeme.jsx
+++ src/pages/CreateMeme.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 const CreateMeme = () => {
   return (
     <div>
-       <h1>Soy createMeme y he venido a crear un meme para ti</h1>
+       <h1>Agregar un meme</h1>
+       <form>
+        <label>Dale un nombre a tu meme</label>
+        <input type="text" placeholder="Nombre"></input>
+        <label>Elige una categoria</label>
+        <select>
+          <option>Seleccion una opción</option>
+          <option value="programation">Programación</option>
+          <option value="work">Trabajo</option>
+          <option value="students">Estudiantes</option>
+        </select>
+        <label>Palabras clave</label>
+        <input type="text" placeholder="Nombre"></input>
+        <label>Subir imagen</label>
+        <input type="file"></input>
+       </form>
     </div>
   )
 }
