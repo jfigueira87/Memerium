81b7430c847f9161f97294aef2520e736d75bab7
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-09-09 00:23:49 +0200

    feat:metodo post datos e imagenes funcional

--- src/pages/CreateMeme.jsx
+++ src/pages/CreateMeme.jsx
@@ -1,26 +1,53 @@
 import React from 'react'
+import { createMeme } from '../services/services';
+import { uploadImage } from '../services/services';
+//import FormMeme from '../components/FormMeme';
 
 const CreateMeme = () => {
   return (
-    <div>
-       <h1>Agregar un meme</h1>
-       <form>
-        <label>Dale un nombre a tu meme</label>
-        <input type="text" placeholder="Nombre"></input>
-        <label>Elige una categoria</label>
-        <select>
-          <option>Seleccion una opción</option>
-          <option value="programation">Programación</option>
-          <option value="work">Trabajo</option>
-          <option value="students">Estudiantes</option>
-        </select>
-        <label>Palabras clave</label>
-        <input type="text" placeholder="Nombre"></input>
-        <label>Subir imagen</label>
-        <input type="file"></input>
-       </form>
-    </div>
-  )
-}
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Agregar un meme</h2>
+      </div>
 
-export default CreateMeme
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form id="formAddMeme" class="space-y-6">
+          <div>
+            <label htmlFor="name" className="block text-sm font-medium leading-6 text-white">Dale un nombre a tu meme</label>
+            <div class="mt-2">
+              <input type="text" name="name" required class="block w-full rounded-md py-1.5 text-black bg-primarylight ring-1 ring-secondary"></input>
+            </div>
+          </div>
+          <div>
+            <label htmlFor="name" className="block text-sm font-medium leading-6 text-white">Elige una categoria</label>
+            <div class="mt-2">
+              <select type="text" name="category" required class="block w-full rounded-md py-1.5 text-black bg-primarylight ring-1 ring-secondary">
+                <option value="" selected> Seleccione una categoria </option>
+                <option value="trabajo">Trabajo</option>
+                <option value="programacion">Programación</option>
+                <option value="estudiante">Estudiante</option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <label htmlFor="keyword" className="block text-sm font-medium leading-6 text-white">Palabras clave</label>
+            <div class="mt-2">
+              <input type="text" name="keyword" required class="block w-full rounded-md py-1.5 text-black bg-primarylight ring-1 ring-secondary"></input>
+            </div>
+          </div>
+          <div>
+            <label htmlFor="photo" className="block text-sm font-medium leading-6 text-white">Subir imagen</label>
+            <div class="mt-2">
+              <input id="photo" type="file" name="photo" onChange={uploadImage}></input>
+            </div>
+          </div>
+          <div>
+            <button onClick={createMeme} class="flex w-full justify-center rounded-md bg-gray-300 px-3 py-1.5 text-sm font-semibold leading-6 text-primary shadow-sm"> Add Image </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateMeme;
--- src/services/services.js
+++ src/services/services.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const URL_API = "http://localhost:3000/memes";
+const URL_STORAGE = "https://api.cloudinary.com/v1_1/dz53okn10/image/upload"; //url del repositorio de imagenes CLOUDINARY
 
 //Get all memes -- GET
 
@@ -14,15 +15,58 @@ export async function getAllMemes() {
 
 //Get one meme by ID -- GET
 
-//Create meme -- POST
-export async function createMeme(bodyMeme) {
-  try {
-    const response = await axios.post(URL_API, bodyMeme)
-    return response.data
-  } catch{
+// async function uploadImage() {
 
+
+//   const response = await fetch("https://api.cloudinary.com/v1_1/dz53okn10/image/upload", {
+//     method: "POST",
+//     body: formData
+//   });
+//   const result = await response.json();
+//   console.log("UPLOAD IMAGE");
+//   fileInput.src = result.secure_url;
+//   console.log(fileInput);
+//   return;
+// }
+
+//Create meme -- POST
+export async function uploadImage() {
+  try {
+    const fileInput = document.querySelector('#photo');
+    const formData = new FormData();
+    formData.append('upload_preset', 'ml_default');
+    formData.append('file', fileInput.files[0]);
+
+    const response = await axios.post(URL_STORAGE, formData)
+    fileInput.src = response.data.secure_url;
+    return response.data;
+  } catch {
+    console.log("ERROR");
   }
 };
+
+export async function createMeme() {
+  try {
+    const fileInput = document.querySelector('#photo');
+    const form = document.querySelector('#formAddMeme');
+
+    const response = await axios.post(URL_API, {
+      name:form.name.value,
+      category:form.category.value,
+      tags:form.keyword.value,
+      url:fileInput.src
+    })
+    .then((response) =>{
+      console.log(response);
+    });
+    return response.data;
+  } catch(e) {console.error("ERROR",e);
+  }
+};
+
+
+
+
 //Delete meme -- DELETE
 export async function deleteMeme(id) {
   try {
