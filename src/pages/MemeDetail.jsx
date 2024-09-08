c9a43a0f68e7bbde4684c2b3d971b74265a8f201
Monica <monicut3@gmail.com>
2024-09-08 22:57:11 +0200

    Add: Add meme details

--- src/pages/MemeDetail.jsx
+++ src/pages/MemeDetail.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useLoaderData } from './react-router-dom'
 
 const MemeDetail = () => {
-  
+  const meme = useLoaderData(); //detalles del meme desde el router
+
   return (
     <div>
       <h1>Aquí puedes ver el detalle del meme</h1>
+      <p>Nombre: {meme.name}</p>
+      <p>Categoría: {meme.category}</p>
+      <p>Url: {meme.url}</p>
+      <p>Etiquetas: {meme.tags.join(',')}</p>
+      <p>Id del meme: {meme.id}</p>
     </div>
-  )
+  );
 }
 
 export default MemeDetail
