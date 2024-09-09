5cd2ef94c60482ff205e7fc0620b164ab56327ab
jfigueira87 <jfigueira@gmail.com>
2024-09-09 14:54:03 +0200

    Fix: Finish Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Card = ({ url }) => {
   return (
-    <div className="w-[330px] h-[348px] bg-[url('./src/assets/images/marcoCard.png')] flex items-center justify-center">
-      <div className="z-50">
-        <img className="w-[280px] h-[310px]" src={url} alt="Painting frame" />
+    <div className='flex items-center justify-center flex-col'>
+      <div className='flex items-center justify-center'>
+        <img src="./src/assets/images/luzCard.png" alt="light" />
+      </div>
+      <div className="w-[330px] h-[348px] bg-[url('./src/assets/images/marcoCard.png')] flex items-center justify-center">
+        <img className="w-[280px] h-[310px] ml-0.5" src={url} alt="Painting frame" />
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,14 +17,9 @@ const Home = () => {
 
   return (
     <>
-      <Card url="https://res.cloudinary.com/dz53okn10/image/upload/v1725878180/Captura_de_pantalla_2024-09-09_123547_ufiara.png"/>
+      
       {memes.map((meme, index) => (
-        <div className="font-[Itim]" key={index}>
-          <p >Nombre del meme: {meme.name}</p>
-          <p>Categoria: {meme.categoria}</p>
-          <img src={meme.url} alt={meme.name} />
-          <p>Tags: {meme.tags}</p>
-        </div>
+        <Card url={meme.url}/>
       ))}
     </>
   );
