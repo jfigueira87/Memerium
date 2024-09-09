ce6a501c12d941127c696b32a865102179050f8e
EnmanuelFL <enmanuellemos.f@gmail.com>
2024-09-09 15:53:12 -0700

    testing the button style

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,9 +4,9 @@ import { Outlet } from 'react-router-dom'
 const Layout = () => {
   return (
     <>
-    <nav>Mi nav</nav>
+  
       <Outlet /> 
-    <footer>Mi footer</footer>
+    
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,13 @@
-import { getAllMemes } from "../services/services";
-import { useEffect, useState } from "react";
 
-const Home = () => {
-  const [memes, setMemes] = useState([]);
+import Buttons from "../components/Buttons";
 
-  const fetchData = async () => {
-    const dataMemes = await getAllMemes();
-    setMemes(dataMemes); // Aquí guardas los datos en el estado `memes`
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+function Home(){
   return (
-    <>
-      <Card url="https://res.cloudinary.com/dz53okn10/image/upload/v1725878180/Captura_de_pantalla_2024-09-09_123547_ufiara.png"/>
-      {memes.map((meme, index) => (
-        <div key={index}>
-          <p>Nombre del meme: {meme.name}</p>
-          <p>Categoria: {meme.categoria}</p>
-          <img src={meme.url} alt={meme.name} />
-          <p>Tags: {meme.tags}</p>
-        </div>
-      ))}
-    </>
-  );
-};
+<>
+<Buttons text="Estudiantes"/>
 
-export default Home;
+</>
+  )
+}
 
+export default Home
