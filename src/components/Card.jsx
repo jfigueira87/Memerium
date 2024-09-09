5044f44a978b53d7f74ebb173128f9da823d562d
jfigueira87 <jfigueira@gmail.com>
2024-09-09 15:30:49 +0200

    Add: dynamic route

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Card = ({ url }) => {
+const Card = ({ url, id_meme }) => {
   return (
     <div className='flex items-center justify-center flex-col'>
       <div className='flex items-center justify-center'>
@@ -9,6 +10,8 @@ const Card = ({ url }) => {
       <div className="w-[330px] h-[348px] bg-[url('./src/assets/images/marcoCard.png')] flex items-center justify-center">
         <img className="w-[280px] h-[310px] ml-0.5" src={url} alt="Painting frame" />
       </div>
+      <Link to={`/detailmeme/${id_meme}`}><button>Ver mas</button></Link>
+      
     </div>
   );
 };
