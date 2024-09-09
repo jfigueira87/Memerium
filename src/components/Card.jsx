a81a73b06ec593c44d18cfe41f006babaa613a82
jfigueira87 <jfigueira@gmail.com>
2024-09-09 14:31:50 +0200

    Fix: Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 const Card = ({ url }) => {
   return (
-    <div className="w-[390px] h-[473px] bg-[url('./src/assets/images/marcoCard.png')]">
+    <div className="w-[330px] h-[348px] bg-[url('./src/assets/images/marcoCard.png')] flex items-center justify-center">
       <div className="z-50">
-        <img src={url} alt="Painting frame" />
+        <img className="w-[280px] h-[310px]" src={url} alt="Painting frame" />
       </div>
     </div>
   );
 };
 
 export default Card;
+
 
