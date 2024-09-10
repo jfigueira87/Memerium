855b31c276549259ab0379398f02916787dcdee6
jfigueira87 <jfigueira@gmail.com>
2024-09-11 00:27:29 +0200

    Add:Print details of the meme passed by URL

--- src/services/services.js
+++ src/services/services.js
@@ -14,6 +14,15 @@ export async function getAllMemes() {
 
 //Get one meme by ID -- GET
 
+export async function getOneMeme(id) {
+  try {
+    const response = await axios.get(`${URL_API}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error en la solicitud:', error);
+  }
+};
+
 //Create meme -- POST
 export async function createMeme(bodyMeme) {
   try {
