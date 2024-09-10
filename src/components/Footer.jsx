fd48a6384f6aae949220316291566969c698dacf
EnmanuelFL <enmanuellemos.f@gmail.com>
2024-09-10 12:40:49 -0700

    Add: footer base

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,15 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      
+        <p>Contacto:445465757</p>
+        <p>Correo electronico:</p>
+      <footer>
+        <img src="./src/assets/icons.memerium/icon.share.png" alt="share" />
+        <img src="./src/assets/icons.memerium/icon.youtube.png" alt="youtube" />
+        <img src="./src/assets/icons.memerium/icon.facebook.png" alt="facebook" />
+
+        <p>©2024 por Memerium </p>
+      </footer>
     </div>
   )
 }
