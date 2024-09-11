d2edf506d736ea6478e580ab030d164d7b3dcb5c
jfigueira87 <jfigueira@gmail.com>
2024-09-11 23:11:21 +0200

    Fix: code format

--- src/pages/CreateMeme.jsx
+++ src/pages/CreateMeme.jsx
@@ -23,24 +23,24 @@ const CreateMeme = () => {
   }
 
   return (
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Agregar un meme</h2>
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Agregar un meme</h2>
       </div>
 
-      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form id="formAddMeme" class="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form id="formAddMeme" className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
           <div>
             <label htmlFor="name" className="block text-sm font-medium leading-6 text-white">Dale un nombre a tu meme</label>
-            <div class="mt-2">
-              <input type="text" {...register('name',{required:true})} class="block w-full rounded-md py-1.5 text-black bg-primarylight ring-1 ring-secondary"></input>
+            <div className="mt-2">
+              <input type="text" {...register('name',{required:true})} className="block w-full rounded-md py-1.5 text-black bg-primarylight ring-1 ring-secondary"></input>
               {errors.name?.type === 'required' && <p>El campo de nombre es requerido</p>}
             </div>
           </div>
           <div>
             <label htmlFor="name" className="block text-sm font-medium leading-6 text-white">Elige una categoria</label>
-            <div class="mt-2">
-              <select type="text" {...register('category',{required:true})} class="block w-full rounded-md py-1.5 text-black bg-primarylight ring-1 ring-secondary">
+            <div className="mt-2">
+              <select type="text" {...register('category',{required:true})} className="block w-full rounded-md py-1.5 text-black bg-primarylight ring-1 ring-secondary">
                 <option value="" selected> Seleccione una categoria </option>
                 <option value="trabajo">Trabajo</option>
                 <option value="programacion">Programación</option>
@@ -51,14 +51,14 @@ const CreateMeme = () => {
           </div>
           <div>
             <label htmlFor="keyword" className="block text-sm font-medium leading-6 text-white">Palabras clave</label>
-            <div class="mt-2">
-              <input type="text" {...register('keyword',{required:true})} class="block w-full rounded-md py-1.5 text-black bg-primarylight ring-1 ring-secondary"></input>
+            <div className="mt-2">
+              <input type="text" {...register('keyword',{required:true})} className="block w-full rounded-md py-1.5 text-black bg-primarylight ring-1 ring-secondary"></input>
               {errors.keyword?.type === 'required' && <p>Es necesario asignar una palabra clave</p>}
             </div>
           </div>
           <div>
             <label htmlFor="url" className="block text-sm font-medium leading-6 text-white">Subir imagen</label>
-            <div class="mt-2">
+            <div className="mt-2">
               <input id="url" type="file" accept='image/*' {...register('url',{
                 required:'Es necesario subir una foto', 
                 validate:{checkFileType: (fileList) => {
