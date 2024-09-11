28d48aaf6919457809a23d48b8d8def05100bf85
Monica <monicut3@gmail.com>
2024-09-11 15:22:30 +0200

    Feat:erase memedetail

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,48 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import{ deleteMeme } from "../services"
-import { getAllMemes } from "axios";
+import{ deleteMeme } from "../services/services.js";
+import { getAllMemes } from "../services/services.js";
 
 
   const Home = () => {
   const [memes, setMemes] = useState([]);
 
   const fetchData = async () => {
-    const dataMemes = await getAllMemes();
-    setMemes(dataMemes); // Aquí guardas los datos en el estado `memes`
-  }
+    try{
+      const response = await getAllmemes ();
+      setMemes (response.data);
+   } catch (error) {
+    console.error ("Error al obtener memes:", error);
+   }
   };
+  //useEffect ejecuta para que el componente funcione
 
-  const handleDelete = async (id) => {
-    await deleteMeme(id); // Eliminamos el meme del servidor
-    setMemes(memes.filter(meme => meme.id !== id)); // Actualiza el estado eliminado
-  }
-
-  useEffect(() => {
+    useEffect(() => {
     fetchData();
   }, []);
 
-    return (
-    
-      <div>
-        <h1> Memerium - Lista de Memes </h1>
-      <ul>
-        {memes.map(meme => (
-          <li key={meme.id} className="meme">
-            <button onClick={() => deleteMeme(meme.id)}>Delete</button>
-          </li>
+  // Ahora DELETE
+  const deleteMeme = async (id) => {
+    try{
+      await deleteMeme(id); // Eliminamos el meme del servidor
+      setMemes(memes.filter(meme => meme.id !== id)); // Actualiza el estado eliminado
+    } catch (error) {
+      console.error ("Error al eliminar el meme: ", error);
+    }
+  }
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 clasName="text-3xl font-bold text-center mb-6"> Memerium - Lista de Memes </h1>
+      <ul className="space-y-4">
+        {memes.map((meme) => (
+          <li key={meme.id} className="bg-white shadow-md rounded-lg p-4 flex justify-between items-center">
+            <span className="text-lg">{meme.name}</span> {}
+            <button onClick={() => deleteMeme(meme.id)} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"> Eliminar </button>
+         </li>
         ))}
         </ul>
-      </div>
-    
+      </div>    
     )
+  }
   export default Home
