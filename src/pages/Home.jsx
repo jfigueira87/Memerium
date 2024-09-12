72f053f2c22905833f678f8937b396389bc77601
jfigueira87 <jfigueira@gmail.com>
2024-09-12 12:27:02 +0200

    Add:home image and about us

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ import Card from "../components/Card";
 
 
 const Home = () => {
+  
+  
+  
   const [memes, setMemes] = useState([]);
 
   const fetchData = async () => {
@@ -18,9 +21,21 @@ const Home = () => {
 
   return (
     <>
-      
-      {memes.map((meme, index) => (
-        <Card url={meme.url} id_meme={meme.id}/>
+      <div className="relative">
+        <img src="src/assets/images/home.png" alt="galeria de memes" className="w-full" />
+        <h2 className="text-white text-6xl absolute bottom-10 left-1/2 transform -translate-x-1/2">
+          Donde los memes se hacen arte
+        </h2>
+      </div>
+      <div className="m-14 text-center">
+        <h2 className="text-3xl text-center font-bold">Sobre nosotros</h2>
+          <p>Memerium, la web de memes que son arte</p>
+          <p>Aquí puedes subir, editar y borrar memes para crear tu propia colección de humor ¡tú decides qué memes viven y cuáles desaparecen!. Ayuda al humor viral: ríete, crea y comparte.</p>
+          <p>Sé parte de Memerium donde cada meme tiene su lugar.</p>
+      </div>
+
+      {memes.map((meme) => (
+        <Card url={meme.url} id_meme={meme.id} />
       ))}
     </>
   );
