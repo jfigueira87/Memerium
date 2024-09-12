6e2adf57f852ec798ac6dccf1d541f6cf947f915
Monica <monicut3@gmail.com>
2024-09-12 09:18:15 +0200

    Add: Add memeDetails

--- src/pages/MemeDetail.jsx
+++ src/pages/MemeDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getOneMeme } from '../services/services';
+import { getOneMeme } from '../services/services.js';
 
 
 const MemeDetail = () => {
