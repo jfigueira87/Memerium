398c017243e8a79dc83aa4ac388d33e4cb57d245
Monica <monicut3@gmail.com>
2024-09-12 17:18:49 +0200

    Styles: Add tailwind to MemeDetail

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,13 +29,14 @@ import{ deleteMeme, getAllMemes } from "../services/services";
   }
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 clasName="text-3xl font-bold text-center mb-6"> Memerium - Lista de Memes </h1>
+    <div className="p-4 max-w-md mx-auto bg-white rounded-lg shadow-md">
+      <h1 className="text-2xl font-delius font-bold text-gray-800 mb-4 sm:text-3xl md:text-4xl"> Memerium - Lista de Memes </h1>
       <ul className="space-y-4">
         {memes.map((meme) => (
-          <li key={meme.id} className="bg-white shadow-md rounded-lg p-4 flex justify-between items-center">
+          <li key={meme.id} className="bg-white p-4 rounded-lg shadow-md text-gray-600 mb-4 text-base sm:text-lg md:text-xl">
             <span className="text-lg">{meme.name}</span> {}
-            <button onClick={() => handleDelete(meme.id)} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"> Eliminar </button>
+            <button onClick={() => handleDelete(meme.id)} className="flex w-full justify-center rounded-md bg-gray-300 px-3 py-1.5 text-sm font-semibold leading-6 text-button-text shadow-sm mt-2"
+            > Eliminar meme </button>
          </li>
         ))}
         </ul>
--- src/pages/MemeDetail.jsx
+++ src/pages/MemeDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import { getOneMeme } from '../services/services';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import { getOneMeme, deleteMeme } from '../services/services';
 
 //Hay que definir el loader y exportar
 export async function loader({ params }){
@@ -12,6 +12,7 @@ const MemeDetail = () => {
   const { id } = useParams(); // Funcion del router dom para obtener el Id de la URL
   const [memeData, setMemeData] = useState(null); // Estado para almacenar los datos del meme
   const [loading, setLoading] = useState(true); // Estado para mostrar si está cargando
+  const navigate = useNavigate ();
 
   useEffect(() => {
     
@@ -29,12 +30,21 @@ const MemeDetail = () => {
     fetchMeme();
   }, [id]);
 
+  const handleDelete = async () => {
+    try {
+      await deleteMeme(id);
+      navigate("/"); //redirige a home
+    } catch (error){
+      console.error("Error al eliminar el meme:", error);
+    }
+  };
+
   if (loading) {
-    return <div>Cargando...</div>; // Mostrar cargando mientras se obtienen los datos
+    return <div className="text-center"> Cargando...</div>; // Mostrar cargando mientras se obtienen los datos
   }
 
   if (!memeData) {
-    return <div>No se encontró el meme</div>; // Mostrar esto si no se encuentra el meme
+    return <div className="flex justify-between items-start p-8 bg-gradient-to-b from-gray-200 to-blue-900 rounded-lg shadow-md max-w-4xl mx-auto mt-12"> No se encontró el meme</div>; // Mostrar esto si no se encuentra el meme
   }
 
   // Formulario con los datos del meme, aún me falta meterle estilo, la imagen del meme y los botones
@@ -82,10 +92,10 @@ const MemeDetail = () => {
           />
         </div>
       </form>
-      <div className="text-center mt-">
-        <Link to="/" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">volver a la Lista
+      <div className="text-center mt-4">
+        <Link to="/" className="flex w-full justify-center rounded-md bg-gray-300 px-3 py-1.5 text-sm font-semibold leading-6 text-primary shadow-sm"> Volver a la Lista
         </Link>
-        <button onClick={handleDelete} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded ml-4">Eliminar
+        <button onClick={handleDelete} className="flex w-full justify-center rounded-md bg-gray-300 px-3 py-1.5 text-sm font-semibold leading-6 text-primary shadow-sm">Eliminar meme
         </button>   
       </div>
     </div>
