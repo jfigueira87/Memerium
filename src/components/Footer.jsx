650263398162803f7ee8be4fdc93f94374ddb7c0
EnmanuelFL <enmanuellemos.f@gmail.com>
2024-09-13 11:10:02 -0700

    Footer Completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,18 +9,19 @@ const Footer = () => {
  
   <div className="lg:flex lg:flex-row lg:justify-start lg:items-start flex flex-col items-center">
    
-    <div className="lg:ml-20 lg:mt-10 text-center lg:text-left lg:text-xl text-sm lg:font-medium font-bold max-md:text-center">
+    <div className="lg:ml-20 lg:mt-0 text-center lg:text-left lg:text-xl text-sm lg:font-medium font-bold max-md:text-center">
       <p>Contacto: 445465757</p>
       <p>Correo electrónico: memento@gmail.com</p>
-         
+    <div className="lg:flex flex lg:mt-6 space-x-3 max-sm:mt-4 max-sm:justify-center lg:space-x-3">   
       <img className="lg:h-10 h-10 lg:" src="src/assets/icons.memrium/🦆 icon.share.png" alt="Icono compartir" />
       <img className="lg:h-10 lg:flex-row h-10 lg:" src="src/assets/icons.memrium/icon.youtube.png" alt="Icono YouTube" />
       <img className="lg:h-10 h-10 lg:" src="src/assets/icons.memrium/🦆 icon .facebook.png" alt="Icono Facebook" />
+    </div>  
     </div>
   </div>
 
  
-  <div className="mt-10 lg:mt-0 text-center lg:text-end lg:ml-auto lg:mr-28 lg:text-xl text-lg font-bold max-md:text-center">
+  <div className="mt-10 lg:-mt-8 text-center lg:text-end lg:ml-auto lg:mr-28 lg:text-xl text-lg font-bold max-md:text-center">
     <p className="text-primary">©2024 por Memerium</p>
   </div>
 </footer>
