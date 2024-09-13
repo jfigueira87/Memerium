bf6ab87b11b0f83e3b8f1df6976feaa569c5372c
Monica <monicut3@gmail.com>
2024-09-13 13:43:16 +0200

    Add: Add tailwind styles in memedetails, form

--- src/pages/MemeDetail.jsx
+++ src/pages/MemeDetail.jsx
@@ -48,111 +48,80 @@ const MemeDetail = () => {
   }
 
   // Formulario con los datos del meme, aún me falta meterle estilo, la imagen del meme y los botones
- 
-//     /*<div className="flex flex-col md:flex-row justify-between items-start p-8 bg-gradient-to-b from-[#A4A5A4] to-[#1E4F64] rounded-lg shadow-md max-w-6xl mx-auto mt-12">
-//       {/* Imagen del meme */}
-//     <div className="w-full md:w-1/2 flex justify-center items-center">
-//     <img
-//     src={memeData.url}
-//     alt={memeData.name}
-//     className="roundes-lg shadow-lg w-full h-auto max-w-sm"
-//     />
-//     </div>
-//     {/*Información del meme*/}
-//     <div className="w-full md:w-1/2 text-white md:pl-12 mt-8 md:mt-0">
-//         <h2 className="text-3xl md:text-4xl font-bold mb-4 font-delius">Título: {memeData.name}</h2>
-        
-//         <p className="text-lg md:text-xl mb-2 font-delius">
-//           <strong>URL:</strong> <a href={memeData.url} className="underline text-gray-300">{memeData.url}</a>
-//         </p>
-        
-//         <p className="text-lg md:text-xl mb-2 font-delius">
-//           <strong>Categoría:</strong> {memeData.category}
-//         </p>
-
-//         <p className="text-lg md:text-xl mb-2 font-delius">
-//           <strong>Palabras claves:</strong> {memeData.tags.join(', ')}
-//         </p>
-
-//         {/* Botones */}
-//         <div className="flex mt-6 space-x-4">
-//           <button
-//             onClick={handleDelete}
-//             className="bg-[#A4A5A4] hover:bg-gray-400 text-[#003340] font-delius font-semibold py-2 px-4 rounded-lg shadow"
-//           >
-//             Eliminar meme
-//           </button>
-          
-//           <Link
-//             to="/"
-//             className="bg-[#A4A5A4] hover:bg-gray-400 text-[#003340] font-delius font-semibold py-2 px-4 rounded-lg shadow"
-//           >
-//             Volver a la Lista
-//           </Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MemeDetail;*/
-
-
-return (
-  <div>
-      <form className="meme-form">
-        <div>
-          <label htmlFor="name">Nombre:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={memeData.name}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="url">URL de la imagen:</label>
-          <input
-            type="text"
-            id="url"
-            name="url"
-            value={memeData.url}
-            />
-        </div>
-
-        <div>
-          <label htmlFor="category">Categoría:</label>
-          <input
-            type="text"
-            id="category"
-            name="category"
-            value={memeData.category}
-            />
-        </div>
-
-        <div>
-          <label htmlFor="tags">Tags:</label>
-          <input
-            type="text"
-            id="tags"
-            name="tags"
-            value={
-              Array.isArray(memeData.tags)
-                ? memeData.tags.join(', ')
-                : (memeData.tags) // En caso de que no sea un array, devuelve la unica palabra que tiene
-            }
-          />
-        </div>
-      </form>
-      <div className="text-center mt-4">
-        <Link to="/" className="flex w-full justify-center rounded-md bg-gray-300 px-3 py-1.5 text-sm font-semibold leading-6 text-primary shadow-sm"> Volver a la Lista
-        </Link>
-        <button onClick={handleDelete} className="flex w-full justify-center rounded-md bg-gray-300 px-3 py-1.5 text-sm font-semibold leading-6 text-primary shadow-sm">Eliminar meme
-        </button>   
+    return (
+      <div className="max-w-4xl mx-auto bg-secondary p-8 rounded-lg shadow-md">
+        <form className="meme-form space-y-4 font-mainFont text-white">
+          <div>
+            <label htmlFor="name" className="block mb-2 text-lg font-semibold">
+              Nombre:
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={memeData.name}
+              className="w-full px-4 py-2 border border-gray-300 rounded-md text-secondary bg-white"
+              />
       </div>
-    </div>
-  );
-};
-
-export default MemeDetail;
+      <div>
+          <label htmlFor="url" className="block mb-2 text-lg font-semibold">
+                URL de la imagen:
+              </label>
+              <input
+                type="text"
+                id="url"
+                name="url"
+                value={memeData.url}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md text-secondary bg-white"
+              />
+            </div>
+      
+            <div>
+              <label htmlFor="category" className="block mb-2 text-lg font-semibold">
+                Categoría:
+              </label>
+              <input
+                type="text"
+                id="category"
+                name="category"
+                value={memeData.category}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md text-secondary bg-white"
+              />
+            </div>
+      
+            <div>
+              <label htmlFor="tags" className="block mb-2 text-lg font-semibold">
+                Tags:
+              </label>
+              <input
+                type="text"
+                id="tags"
+                name="tags"
+                value={
+                  Array.isArray(memeData.tags)
+                    ? memeData.tags.join(", ")
+                    : memeData.tags
+                }
+                className="w-full px-4 py-2 border border-gray-300 rounded-md text-secondary bg-white"
+              />
+            </div>
+          </form>
+      
+          <div className="text-center mt-8 space-y-4">
+            <Link
+              to="/"
+              className="inline-block w-full md:w-1/2 bg-colorButton text-primary font-semibold py-2 px-4 rounded-md shadow-md hover:bg-gray-400"
+            >
+              Volver a la Lista
+            </Link>
+            <button
+              onClick={handleDelete}
+              className="inline-block w-full md:w-1/2 bg-colorButton text-primary font-semibold py-2 px-4 rounded-md shadow-md hover:bg-gray-400"
+            >
+              Eliminar meme
+            </button>
+          </div>
+        </div>
+      )
+    };
+      export default MemeDetail;
