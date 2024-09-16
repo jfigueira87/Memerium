e855774377d9f1181df5a845fe06423d48656666
Monica <monicut3@gmail.com>
2024-09-16 22:37:44 +0200

    Feat: clone nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,11 +5,6 @@ import { EnvelopeIcon } from '@heroicons/react/24/outline';
 import { Bars3Icon, HomeIcon, UsersIcon, XMarkIcon } from '@heroicons/react/24/solid'
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
-import { PhotoIcon } from '@heroicons/react/20/solid';
-//import { EnvelopeIcon } from '@heroicons/react/24/outline';
-//import { Bars3Icon, HomeIcon, UsersIcon, XMarkIcon } from '@heroicons/react/24/solid'
-//import { Link } from 'react-router-dom';
-//import { HashLink } from 'react-router-hash-link';
 
 
 
@@ -19,7 +14,7 @@ function classNames(...classes) {
 
 export default function Navbar() {
     return (
-        <Disclosure as="nav" className="bg-white text-primary font-mainFont font-bold top-0 left-0 w-full sticky z-10">
+        <Disclosure as="nav" className="bg-white text-primary font-mainFont font-bold top-0 left-0 w-full sticky z-50">
             <div className="mx-auto max-w-7xl px-2 md:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 right-0 flex items-center md:hidden">
@@ -27,8 +22,8 @@ export default function Navbar() {
                         <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-primary  hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white z-10">
                             <span className="absolute -inset-0.5" />
                             <span className="sr-only">Open main menu</span>
-                            <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden z-30" />
-                            <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block z-10" />
+                            <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden z-50" />
+                            <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block z-50" />
                         </DisclosureButton>
                     </div>
                     <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start text-xl">
@@ -43,12 +38,9 @@ export default function Navbar() {
                         <Menu as="div" className="relative ml-3 lg:text-primary text-white flex text-sm md:ml-6 md:block md:bg-white ">
                             <div className="flex space-x-12">
                                 <HashLink smooth to="/#home">Inicio</HashLink>
-                                <HashLink smooth to="/#home">Inicio</HashLink>
                                 <MenuButton className="relative">
                                     <span>Galerias</span>
                                 </MenuButton>
-                                <HashLink smooth to="/#aboutUs">Sobre Nosotros</HashLink>
-                                <HashLink smooth to="#contact">Contacto</HashLink>
                                 <HashLink smooth to="/#aboutUs">Sobre Nosotros</HashLink>
                                 <HashLink smooth to="#contact">Contacto</HashLink>
                             </div>
@@ -59,7 +51,7 @@ export default function Navbar() {
 
                                 <Link to={`/gallerywork`}>
                                     <MenuItem>
-                                        <a href="#" className="text-white bg-secondary block px-4 py-3 text-center">
+                                        <a href="#" className="text-white bg-primary block px-4 py-3 text-center">
                                             memes de trabajo
                                         </a>
                                     </MenuItem>
@@ -89,34 +81,50 @@ export default function Navbar() {
             </div>
 
             <DisclosurePanel className="md:hidden bg-primary text-white w-full h-screen place-content-evenly">
-                <div className="flex flex-col items-center text-center space-y-16">
-                    <DisclosureButton as="a" href="/#home">
-                        <HomeIcon className="size-14 group-data-[open]:block" ></HomeIcon>Inicio
-                    </DisclosureButton>
-                    <DisclosureButton as="a" href="/#contact">
-                        <PhotoIcon className="size-14 group-data-[open]:block" ></PhotoIcon>Galerias
-                    </DisclosureButton>
-                    <DisclosureButton as="a" href="#aboutUs" className="flex flex-col items-center">
-                        <UsersIcon className='size-14 group-data-[open]:block'></UsersIcon>Sobre Nosotros
-                    </DisclosureButton>
-                    <DisclosureButton as="a" href="#contact">
-                        <EnvelopeIcon className='size-14 group-data-[open]:block'></EnvelopeIcon>Contacto
-            <DisclosurePanel className="md:hidden bg-primary text-white w-full h-screen place-content-evenly">
-                <div className="flex flex-col items-center text-center space-y-16">
-                    <DisclosureButton as="a" href="/#home">
-                        <HomeIcon className="size-14 group-data-[open]:block" ></HomeIcon>Inicio
-                    </DisclosureButton>
-                    <DisclosureButton as="a" href="/#contact">
-                        <PhotoIcon className="size-14 group-data-[open]:block" ></PhotoIcon>Galerias
-                    </DisclosureButton>
-                    <DisclosureButton as="a" href="#aboutUs" className="flex flex-col items-center">
-                        <UsersIcon className='size-14 group-data-[open]:block'></UsersIcon>Sobre Nosotros
-                    </DisclosureButton>
-                    <DisclosureButton as="a" href="#contact">
-                        <EnvelopeIcon className='size-14 group-data-[open]:block'></EnvelopeIcon>Contacto
-                    </DisclosureButton>
+                <div className="flex flex-col items-center text-center">
+                    <Menu as="div" className="ml-3 lg:text-primary text-white flex flex-col text-sm md:ml-6 md:block md:bg-white space-y-16">
+                        <DisclosureButton as="a" href="/#home" className="flex flex-col items-center">
+                            <HomeIcon className="size-14 group-data-[open]:block" ></HomeIcon>Inicio
+                        </DisclosureButton>
+                        <MenuButton as="a" className="flex flex-col items-center">
+                            <span><PhotoIcon className="size-14 group-data-[open]:block" ></PhotoIcon>Galerias</span>
+                        </MenuButton>
+                        <DisclosureButton as="a" href="/#aboutUs" className="flex flex-col items-center">
+                            <UsersIcon className='size-14 group-data-[open]:block'></UsersIcon>Sobre Nosotros
+                        </DisclosureButton>
+                        <DisclosureButton as="a" href="#contact" className="flex flex-col items-center">
+                            <EnvelopeIcon className='size-14 group-data-[open]:block'></EnvelopeIcon>Contacto
+                        </DisclosureButton>
+                        <MenuItems
+                            transition
+                            className="absolute -translate-x-8 translate-y-40 w-">
+
+                            <Link to={`/gallerywork`}>
+                                <MenuItem>
+                                    <DisclosureButton href="#galleryWork" className="text-white bg-secondary block px-2 py-2 text-center group-data-[open]:block w-40">
+                                        <PhotoIcon className='size-6'></PhotoIcon>trabajo
+                                    </DisclosureButton>
+                                </MenuItem>
+                            </Link>
+                            <Link to={`/galleryprogram`}>
+                                <MenuItem>
+                                    <DisclosureButton href="#galleryProgram" className="text-primary bg-white block px-2 py-2 text-center group-data-[open]:block w-40">
+                                        <PhotoIcon className='size-6'></PhotoIcon>programación
+                                    </DisclosureButton>
+                                </MenuItem>
+                            </Link>
+                            <Link to={`/gallerystudent`}>
+                                <MenuItem>
+                                    <DisclosureButton href="#galleryStudent" className="text-white bg-secondary block px-2 py-2 text-center group-data-[open]:block w-40">
+                                        <PhotoIcon className='size-6'></PhotoIcon>estudiante
+                                    </DisclosureButton>
+                                </MenuItem>
+                            </Link>
+                        </MenuItems>
+                    </Menu>
                 </div>
             </DisclosurePanel>
-        </Disclosure>
-    );
+
+        </Disclosure >
+    )
 }
