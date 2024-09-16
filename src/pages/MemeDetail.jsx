6d241d5b0c0e2740ab4a46b671f1b21693d50812
Monica <monicut3@gmail.com>
2024-09-16 14:21:13 +0200

    Style:Add responsive and add link to edit information in MemeDetail

--- src/pages/MemeDetail.jsx
+++ src/pages/MemeDetail.jsx
@@ -1,12 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
-import { getOneMeme, deleteMeme } from "../services/services";
-
-//Hay que definir el loader y exportar
-export async function loader({ params }) {
-  const data = await getOneMeme(params.id);
-  return { meme: data };
-}
+import React, { useEffect, useState } from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import { getOneMeme, deleteMeme } from '../services/services';
 
 const MemeDetail = () => {
   const { id } = useParams(); // Funcion del router dom para obtener el Id de la URL
@@ -41,7 +35,6 @@ const MemeDetail = () => {
   if (loading) {
     return (
       <div className="text-center font-delius text-lg text-gray-500">
-        {" "}
         Cargando...
       </div>
     ); // Mostrar cargando mientras se obtienen los datos
@@ -50,37 +43,34 @@ const MemeDetail = () => {
   if (!memeData) {
     return (
       <div className="text-center font-delius text-lg text-red-500">
-        {" "}
         No se encontró el meme
       </div>
     ); // Mostrar esto si no se encuentra el meme
   }
 
-  // Formulario con los datos del meme, aún me falta meterle estilo, la imagen del meme y los botones
   return (
-    <div className="min-h-screen w-full flex flex-col justify-center bg-secondary">
+    <div className="min-h-screen w-full flex flex-col items-center bg-secondary py-8">
       
       {/* Contenedor principal */}
-      <div className="max-w-6xl w-full mx-auto p-8 flex flex-col md:flex-row justify-between items-center space-y-8 md:space-y-0">
-        
+      <div className="max-w-6xl w-full px-4 md:px-8 flex flex-col md:flex-row justify-between items-center space-y-8 md:space-y-0">
+        /*
         {/* Contenedor para la imagen con el marco */}
         <div className="relative w-full md:w-1/2 flex justify-center">
           <div
-            className="p-4 rounded-lg shadow-lg w-full md:w-[350px] bg-no-repeat bg-center"
+            className="p-4 rounded-lg shadow-lg w-full md:w-[350px] bg-no-repeat bg-center bg-cover"
             style={{
               backgroundImage: "url('../src/assets/images/marcoCard.png')", // Ruta del archivo del marco
-              backgroundSize: "100% 100%", // Hace que el marco se ajuste al contenedor
             }}
           >
             <img
               src={memeData.url}
               alt={memeData.name}
-              className="w-full h-auto"
+              className="w-full h-auto object-cover"
             />
           </div>
   
           {/* Lámpara encima de la imagen */}
-          <div className="absolute -top-24 left-1/2 transform -translate-x-1/2 w-36">
+          <div className="absolute -top-24 left-1/2 transform -translate-x-1/2 w-36 md:w-48">
             <img
               src="../src/assets/images/luzCard.png" // Ruta del archivo de la lámpara
               alt="Lamp"
@@ -91,24 +81,22 @@ const MemeDetail = () => {
   
         {/* Información del meme */}
         <div className="w-full md:w-1/2 text-white font-mainFont space-y-4">
-          <h2 className="text-xl font-semibold">Título: {memeData.name}</h2>
-          <p className="text-lg">URL: {memeData.url}</p>
+          <h2 className="text-xl md:text-2xl font-semibold">Título: {memeData.name}</h2>
+          <p className="text-lg break-words">URL: <span className="break-all">{memeData.url}</span></p>
           <p className="text-lg">Categoría: {memeData.category}</p>
           <p className="text-lg">Palabras claves: {Array.isArray(memeData.tags) ? memeData.tags.join(', ') : memeData.tags}</p>
         
           {/* Botones */}
-          <div className="flex space-x-4 mt-4">
+          <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mt-4">
             <button
               onClick={handleDelete}
-              className="w-1/2 bg-colorButton text-primary font-semibold py-2 px-4 rounded-md shadow-md hover:bg-gray-400"
-            >
+              className="w-full md:w-1/2 bg-colorButton text-primary font-semibold py-2 px-4 rounded-md shadow-md hover:bg-gray-400">
               Eliminar meme
             </button>
             <Link
-              to="/"
-              className="w-1/2 bg-colorButton text-primary font-semibold py-2 px-4 rounded-md shadow-md hover:bg-gray-400 text-center"
-            >
-              Volver a la lista
+              to={`/editmeme/${id}`}
+              className="w-full md:w-1/2 bg-colorButton text-primary font-semibold py-2 px-4 rounded-md shadow-md hover:bg-gray-400 text-center">
+              Editar Información
             </Link>
           </div>
         </div>
@@ -116,5 +104,6 @@ const MemeDetail = () => {
     </div>
   );
 }
-export default MemeDetail  
-  
+
+export default MemeDetail;
+
